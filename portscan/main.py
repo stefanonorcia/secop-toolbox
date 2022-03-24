5b173e50d18cb8fa57febce04069c032db84a570
nicolasiurich <89192491+nicolasiurich@users.noreply.github.com>
2022-03-24 15:46:53 +0100

    Delete main.py

--- portscan/main.py
+++
@@ -1,9 +0,0 @@
-import socket
-for port in range(1, 100 ):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    risultati = sock.connect_ex(("192.168.1.98", port))
-    if risultati == 0:
-        print("La porta " + str(port) + " è aperta ")
-    else:
-        print("La porta " + str(port) + " è chiusa ")
-    sock.close()
