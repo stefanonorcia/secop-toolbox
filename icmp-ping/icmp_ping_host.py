ac0c6084d30dd2bdc7715d5bda230178c276844d
franzo9448 <andronico.francesco@gmail.com>
2022-03-25 17:01:52 +0100

    tcp-syn-ping

--- icmp-ping/icmp_ping_host.py
+++ icmp-ping/icmp_ping_host.py
@@ -9,8 +9,9 @@ if response == 0:
 else:
   print(hostname, 'is down!')"""
 import sys
-from scapy.all import sr1,IP,ICMP
+from scapy.layers.inet import IP
+from scapy.sendrecv import sr1
 
-p=sr1(IP(dst=sys.argv[1])/ICMP())
+p = sr1(IP(dst=sys.argv[1])/ICMP())
 if p:
     p.show()
--- tcp-ack-ping/ackping.py
+++ tcp-ack-ping/ackping.py
@@ -1,10 +1,12 @@
 import sys
 import scapy.all as scapy
 
+
 def tcp_ping(host, port):
     ''' TCP Ping '''
     ans, unans = scapy.sr(scapy.IP(dst=host) / scapy.TCP(dport=port, flags="S"), timeout=0.1)
     for snd, rcv in ans:
         rcv.sprintf(r"%Ether.src% & %ARP.psrc%\\")
+
 
 tcp_ping(sys.argv[1], int(sys.argv[2]))
