3e679388fa9fd9eee26d78e9ca2d731cf583be45
Stefano Norcia <stefano.norcia@gmail.com>
2022-03-25 17:01:54 +0100

    refactor

--- portscan/main.py
+++
@@ -1,9 +0,0 @@
-import socket
-for port in range(1, 100 ):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    risultati = sock.connect_ex(("127.0.0.1", port))
-    if risultati == 0:
-        print("La porta " + str(port) + " è aperta ")
-    else:
-        print("La porta " + str(port) + " è chiusa ")
-    sock.close()
